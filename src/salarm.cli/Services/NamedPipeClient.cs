d0b1865712eb05880eb981f21a52ca534c6f866f
btigi <btigi@users.noreply.github.com>
2025-06-14 16:13:17 +0100

    Tidy up named pipe client

--- src/salarm.cli/Services/NamedPipeClient.cs
+++ src/salarm.cli/Services/NamedPipeClient.cs
@@ -8,26 +8,18 @@ namespace salarm.cli.services;
 
 public class NamedPipeClient : IAlarmClient
 {
-    public async Task<Alarm> SetAlarm(TimeSpan duration, string? soundFilePath = null, string? message = null)
+    private const string PipeName = "salarm_pipe";
+    private const int ConnectionTimeout = 5000; // 5 seconds
+
+    private async Task<T> SendCommandAsync<T>(Command command)
     {
-        using var pipeClient = new NamedPipeClientStream(".", "salarm_pipe", PipeDirection.InOut);
+        using var pipeClient = new NamedPipeClientStream(".", PipeName, PipeDirection.InOut);
         
         try
         {
-            await pipeClient.ConnectAsync(5000); // 5 second timeout
+            await pipeClient.ConnectAsync(ConnectionTimeout);
 
-            var request = new Command
-            {
-                Action = "setalarm",
-                Alarm = new Alarm
-                {
-                    TriggerTime = DateTime.Now.Add(duration),
-                    SoundFilePath = soundFilePath,
-                    Message = message
-                }
-            };
-
-            var requestJson = JsonSerializer.Serialize(request);
+            var requestJson = JsonSerializer.Serialize(command);
             var requestBytes = Encoding.UTF8.GetBytes(requestJson);
             await pipeClient.WriteAsync(requestBytes, 0, requestBytes.Length);
 
@@ -42,99 +34,60 @@ public class NamedPipeClient : IAlarmClient
 
             try
             {
-                var alarm = JsonSerializer.Deserialize<Alarm>(response);
-                if (alarm != null)
+                var result = JsonSerializer.Deserialize<T>(response);
+                if (result == null)
                 {
-                    return alarm;
+                    throw new Exception("Server returned null response");
                 }
+                return result;
             }
             catch (JsonException)
             {
                 throw new Exception($"Server returned invalid response: {response}");
             }
-            
-            throw new Exception("Failed to set alarm");
         }
         catch (TimeoutException)
         {
             throw new Exception("Could not connect to alarm service. Make sure the WPF application is running.");
         }
+    }
+
+    public async Task<Alarm> SetAlarm(TimeSpan duration, string? soundFilePath = null, string? message = null)
+    {
+        var command = new Command
+        {
+            Action = "setalarm",
+            Alarm = new Alarm
+            {
+                TriggerTime = DateTime.Now.Add(duration),
+                SoundFilePath = soundFilePath,
+                Message = message
+            }
+        };
+
+        return await SendCommandAsync<Alarm>(command);
     }
 
     public async Task<IEnumerable<Alarm>> GetActiveAlarms()
     {
-        using var pipeClient = new NamedPipeClientStream(".", "salarm_pipe", PipeDirection.InOut);
-        
-        try
+        var command = new Command
         {
-            await pipeClient.ConnectAsync(5000); // 5 second timeout
+            Action = "getactivealarms"
+        };
 
-            var request = new Command
-            {
-                Action = "getactivealarms"
-            };
-
-            var requestJson = JsonSerializer.Serialize(request);
-            var requestBytes = Encoding.UTF8.GetBytes(requestJson);
-            await pipeClient.WriteAsync(requestBytes, 0, requestBytes.Length);
-
-            var buffer = new byte[4096];
-            var bytesRead = await pipeClient.ReadAsync(buffer, 0, buffer.Length);
-            var response = Encoding.UTF8.GetString(buffer, 0, bytesRead);
-            
-            if (response.StartsWith("Error:"))
-            {
-                throw new Exception(response);
-            }
-
-            try
-            {
-                var alarms = JsonSerializer.Deserialize<IEnumerable<Alarm>>(response);
-                return alarms ?? Enumerable.Empty<Alarm>();
-            }
-            catch (JsonException)
-            {
-                throw new Exception($"Server returned invalid response: {response}");
-            }
-        }
-        catch (TimeoutException)
-        {
-            throw new Exception("Could not connect to alarm service. Make sure the WPF application is running.");
-        }
+        var result = await SendCommandAsync<IEnumerable<Alarm>>(command);
+        return result ?? Enumerable.Empty<Alarm>();
     }
 
     public async Task<bool> CancelAlarm(Guid alarmId)
     {
-        using var pipeClient = new NamedPipeClientStream(".", "salarm_pipe", PipeDirection.InOut);
-        
-        try
+        var command = new Command
         {
-            await pipeClient.ConnectAsync(5000); // 5 second timeout
+            Action = "cancelalarm",
+            AlarmId = alarmId
+        };
 
-            var request = new Command
-            {
-                Action = "cancelalarm",
-                AlarmId = alarmId
-            };
-
-            var requestJson = JsonSerializer.Serialize(request);
-            var requestBytes = Encoding.UTF8.GetBytes(requestJson);
-            await pipeClient.WriteAsync(requestBytes, 0, requestBytes.Length);
-
-            var buffer = new byte[4096];
-            var bytesRead = await pipeClient.ReadAsync(buffer, 0, buffer.Length);
-            var response = Encoding.UTF8.GetString(buffer, 0, bytesRead);
-            
-            if (response.StartsWith("Error:"))
-            {
-                throw new Exception(response);
-            }
-
-            return response.Contains("successfully");
-        }
-        catch (TimeoutException)
-        {
-            throw new Exception("Could not connect to alarm service. Make sure the WPF application is running.");
-        }
+        var response = await SendCommandAsync<string>(command);
+        return response.Contains("successfully");
     }
 }
